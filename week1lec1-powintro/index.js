9c22e349e8df56942879b1076df40ccd52322fcc
delbyte <blazed0utcode@gmail.com>
2025-05-30 16:06:06 +0530

    Fix console log to display results from findHashWithLeadingZeros function

--- week1lec1-powintro/index.js
+++ week1lec1-powintro/index.js
@@ -1,26 +1,23 @@
 //1
 const crypto = require('crypto');
 
-const input = '0';
+var input = '0';
 var hash = crypto.createHash('sha256').update(input).digest('hex');
 
 console.log(hash)
 
 //2
-function findHashWithStart(prefix) {
+function findHashWithLeadingZeros(zeros = 5) {
     let input = '0';
+    let hash = crypto.createHash('sha256').update(input).digest('hex');
+    const prefix = '0'.repeat(zeros);
 
-    while(true) {
-        let inputStr = input.toString();
-        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
-
-        if(hash.startsWith(prefix)){
-            return { input: inputStr, hash: hash};
-        }
-        input++;
+    while (!hash.startsWith(prefix)) {
+        input = String(Number(input) + 1);
+        hash = crypto.createHash('sha256').update(input).digest('hex');
     }
+    return { input, hash };
 }
 
-const result = findHashWithStart('00000');
-console.log('Input: ', result.input);
-console.log('Hash: ', result.hash);
+console.log('Input:', result.input);
+console.log('Hash:', result.hash);
