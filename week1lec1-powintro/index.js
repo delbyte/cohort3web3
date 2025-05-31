8e7fad7bdb831296de532bf7c1eca55892dc8dc5
delbyte <blazed0utcode@gmail.com>
2025-05-31 23:00:06 +0530

    Refactor hashing logic to include prefix and clean up commented code

--- week1lec1-powintro/index.js
+++ week1lec1-powintro/index.js
@@ -1,12 +1,14 @@
 //1
-const crypto = require('crypto');
 
+const crypto = require('crypto');
+/*
 var input = '0';
 var hash = crypto.createHash('sha256').update(input).digest('hex');
 
 console.log(hash)
 
 //2
+
 function findHashWithLeadingZeros(zeros = 5) {
     let input = '0';
     let hash = crypto.createHash('sha256').update(input).digest('hex');
@@ -19,5 +21,24 @@ function findHashWithLeadingZeros(zeros = 5) {
     return { input, hash };
 }
 
+
 console.log('Input:', result.input);
 console.log('Hash:', result.hash);
+*/
+//3
+
+const prefix = '100xdevs';
+
+var input = '0'
+
+var comb = prefix + input;
+
+hash = crypto.createHash('sha256').update(input).digest('hex');
+
+while (!hash.startsWith('00000')) {
+    input = String(Number(input) + 1);
+    comb = prefix + input;
+    hash = crypto.createHash('sha256').update(comb).digest('hex');
+}
+console.log("Hash:", hash);
+console.log("Input:", comb);
